8f2db0cc6f0cf02e3d0ec7374e5475a02dd6d90f
Swapnil Ahirwar <swapnil.ahirwar@nextlabsonline.com>
2024-12-02 11:16:12 +0530

    first commit

---
+++ assets/scripts/assignment_1.js
@@ -0,0 +1,12 @@
+const userInput = 10;
+
+let result;
+
+result = userInput + 18;
+
+result = result - 8;
+result = result * 8;
+result = result/2;
+
+alert('the result is', result);
+alert('the user input is', userInput);
